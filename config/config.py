e4f1c2c44f1a273e4eade37a431f1c4ef5e0ce07
imranseyaccount <Imran.S@in.ey.com>
2026-03-27 18:27:17 +0530

    this is first commit

--- config/config.py
+++
@@ -1,6 +0,0 @@
-from config.logger import get_logger
-
-logger = get_logger(__name__)
-
-BASE_URL = "https://jsonplaceholder.typicode.com"
-logger.debug(f"Configuration loaded - BASE_URL: {BASE_URL}")
